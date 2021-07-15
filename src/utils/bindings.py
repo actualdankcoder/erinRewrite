902910a1a9f6c2f620a947afd7ff3f5ba453d83e
UnsignedArduino <UnsignedArduino@outlook.com>
2021-07-15 16:37:45 -0400

    Rename collections and database

--- src/utils/bindings.py
+++ src/utils/bindings.py
@@ -1,8 +1,8 @@
 import asyncio
 
-import motor.motor_asyncio
 from discord.ext import tasks
 from lru import LRU
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from singleton import Singleton
 from task import *
@@ -17,17 +17,18 @@ class CacheManger:
         self.cache = LRU(cache_size)
 
 
-class ErinDatabase(metaclass=Singleton):
+class AsyncErinDatabase(metaclass=Singleton):
     """
     Responsible for making database requests across all cogs
     An instance of this class would be globally accessible via src/globals.py
     """
 
     def __init__(self, URI: str):
-        self.database = motor.motor_asyncio.AsyncIOMotorCLient(URI)
-        self.economy = self.database["economy"]
-        self.cooldowns = self.database["cooldowns"]
-        self.queue = asyncio.Queue()
+        self.database = AsyncIOMotorClient(URI)
+        self.erin_db = self.database["erin_rewrite"]
+        self.users_db = self.erin_db["users"]
+        self.guild_db = self.erin_db["guilds"]
+        self.task_queue = asyncio.Queue()
         self.cache = CacheManger()
 
     async def new_task(self, task: Task):
@@ -35,7 +36,7 @@ class ErinDatabase(metaclass=Singleton):
         Add tasks to queue flow, a background task would be responsible for
         executing these at set intervals of time
         """
-        await self.queue.put(task)
+        await self.task_queue.put(task)
 
     @tasks.loop(seconds=0.5)
     async def task_loader(self):
@@ -43,4 +44,4 @@ class ErinDatabase(metaclass=Singleton):
         This task loader is responsible for making database calls for the given
         jobs
         """
-        task = await self.queue.get()
+        task = await self.task_queue.get()
