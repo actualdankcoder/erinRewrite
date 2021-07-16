2bcb0ca37a408d92bd6f60605ebd194247a195d6
UnsignedArduino <UnsignedArduino@outlook.com>
2021-07-15 20:06:17 -0400

    Can get prefix async (not tested)

--- src/utils/bindings.py
+++ src/utils/bindings.py
@@ -1,11 +1,11 @@
 import asyncio
+from enum import Enum
+from typing import Union
 
-from discord.ext import tasks
 from lru import LRU
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from singleton import Singleton
-from task import *
 
 
 class CacheManger:
@@ -17,31 +17,51 @@ class CacheManger:
         self.cache = LRU(cache_size)
 
 
+class UserKeys(Enum):
+    pass
+
+
+def get_blank_user_template() -> dict:
+    user_template = {
+
+    }
+    return user_template.copy()
+
+
+class GuildKeys(Enum):
+    PREFIXES = "prefixes"
+
+
+def get_blank_guild_template() -> dict:
+    guild_template = {
+        GuildKeys.PREFIXES: ["-"]
+    }
+    return guild_template.copy()
+
+
 class AsyncErinDatabase(metaclass=Singleton):
     """
     Responsible for making database requests across all cogs
-    An instance of this class would be globally accessible via src/globals.py
+    (AsyncErinDatabase is a singleton so you can instantiate in each cog
+    and it won't create more connections)
     """
 
     def __init__(self, URI: str):
         self.database = AsyncIOMotorClient(URI)
         self.erin_db = self.database["erin_rewrite"]
-        self.users_db = self.erin_db["users"]
-        self.guild_db = self.erin_db["guilds"]
+        self.users_col = self.erin_db["users"]
+        self.guild_col = self.erin_db["guilds"]
         self.task_queue = asyncio.Queue()
         self.cache = CacheManger()
 
-    async def new_task(self, task: Task):
-        """
-        Add tasks to queue flow, a background task would be responsible for
-        executing these at set intervals of time
-        """
-        await self.task_queue.put(task)
+    async def register_user_if_needed(self, user_id: Union[int, str]):
+        if await self.users_col.find_one(str(user_id)) is None:
+            await self.users_col.insert_one(get_blank_user_template())
 
-    @tasks.loop(seconds=0.5)
-    async def task_loader(self):
-        """
-        This task loader is responsible for making database calls for the given
-        jobs
-        """
-        task = await self.task_queue.get()
+    async def register_guild_if_needed(self, guild_id: Union[int, str]):
+        if await self.guild_col.find_one(str(guild_id)) is None:
+            await self.guild_col.insert_one(get_blank_guild_template())
+
+    async def get_prefix(self, guild_id: Union[int, str]) -> list[str]:
+        await self.register_guild_if_needed(guild_id)
+        return self.guild_col.find_one(str(guild_id))[GuildKeys.PREFIXES]
