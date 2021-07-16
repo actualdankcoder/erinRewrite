f2538cff33d02b87337798d564948b84789d0fcc
actualdankcoder <zubi2048@gmail.com>
2021-07-16 12:21:14 +0530

    pymongo -> motor

--- src/utils/bindings.py
+++ src/utils/bindings.py
@@ -2,7 +2,7 @@ import logging
 from typing import List
 
 from lru import LRU
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from .create_logger import create_logger
 from .singleton import Singleton
@@ -44,35 +44,40 @@ class ErinDatabase(metaclass=Singleton):
 
     def __init__(self, URI: str):
         logger.debug("Starting MongoDB connection")
-        self.database = MongoClient(URI)
+        self.database = AsyncIOMotorClient(URI)
         self.erin_db = self.database.erin_rewrite
         self.users_col = self.erin_db["users"]
         self.guild_col = self.erin_db["guilds"]
         self.cache_manager = CacheManger()
 
-    def register_user_if_needed(self, user_id: int):
+    async def register_user_if_needed(self, user_id: int):
         user_id = str(user_id)
-        if self.users_col.find_one({"id": user_id}) is None:
+        callback=await self.users_col.find_one({"id": user_id})
+        if callback is None:
             doc = get_blank_user_template()
             doc["id"] = user_id
-            self.users_col.insert_one(doc)
+            await self.users_col.insert_one(doc)
             logger.debug(f"User ID {user_id} has been registered!")
 
-    def register_guild_if_needed(self, guild_id: int):
+    async def register_guild_if_needed(self, guild_id: int):
         guild_id = str(guild_id)
 
         # Verify if the guild prefix was cached before
         if self.cache_manager.prefix_cache.has_key(guild_id):
             return self.cache_manager.prefix_cache[guild_id]
-
-        if self.guild_col.find_one({"id": guild_id}) is None:
+            
+        callback=await self.guild_col.find_one({"id": guild_id})
+        if callback is None:
             doc = get_blank_guild_template()
             doc["id"] = guild_id
-            self.guild_col.insert_one(doc)
+            await self.guild_col.insert_one(doc)
             self.cache_manager.prefix_cache[guild_id]=doc["prefixes"]
             logger.debug(f"Guild ID {guild_id} has been registered!")
             return doc["prefixes"]
+        else:
+            self.cache_manager.prefix_cache[guild_id]=callback["prefixes"]
+            return callback["prefixes"]
 
-    def get_prefix(self, guild_id: int) -> List[str]:
+    async def get_prefix(self, guild_id: int) -> List[str]:
         
-        return self.register_guild_if_needed(guild_id)
+        return await self.register_guild_if_needed(guild_id)
