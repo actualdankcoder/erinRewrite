304ce8b64ccccb7f7d8d3c842b062540d643acef
UnsignedArduino <UnsignedArduino@outlook.com>
2021-07-19 18:22:53 -0400

    Can add and remove prefixes from each guild (untested)

--- src/utils/bindings.py
+++ src/utils/bindings.py
@@ -6,9 +6,20 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from src.utils.cacher import CacheManger
 from src.utils.create_logger import create_logger
 from src.utils.singleton import Singleton
+from src.utils.exceptions import *
+from string import ascii_letters, digits, punctuation
 
 # Configure logger
 logger = create_logger(name=__file__, level=logging.DEBUG)
+
+# Define a gazillion constants
+MAX_PREFIX_LENGTH = 1
+MIN_PREFIX_LENGTH = 1
+
+MAX_PREFIXES_PER_GUILD = 3
+MIN_PREFIXES_PER_GUILD = 1
+
+ALLOWED_PREFIXES = list(ascii_letters + digits + punctuation)
 
 
 def get_blank_user_template() -> dict:
@@ -52,6 +63,10 @@ class AsyncErinDatabase(metaclass=Singleton):
         else:
             return callback
 
+    async def set_user(self, user_id: str, new_doc: dict):
+        await self.users_col.replace_one({"id": user_id}, new_doc,
+                                         upsert=True)
+
     async def get_guild(self, guild_id: str) -> dict:
         callback = await self.guild_col.find_one({"id": guild_id})
         if callback is None:
@@ -63,5 +78,48 @@ class AsyncErinDatabase(metaclass=Singleton):
         else:
             return callback
 
+    async def set_guild(self, guild_id: str, new_doc: dict):
+        await self.guild_col.replace_one({"id": guild_id}, new_doc,
+                                         upsert=True)
+
     async def get_prefix(self, guild_id: int) -> List[str]:
         return (await self.get_guild(str(guild_id)))["prefixes"]
+
+    async def add_prefix(self, guild_id: int, new_prefix: str):
+        guild = await self.get_guild(str(guild_id))
+        prefixes = guild["prefixes"]
+        if len(new_prefix) > MAX_PREFIX_LENGTH:
+            raise TooLongPrefix(f"The prefix \"{new_prefix}\" is too long! "
+                                f"(the prefix must be less then or equal to"
+                                f"{MAX_PREFIX_LENGTH} character(s))")
+        if len(new_prefix) < MIN_PREFIX_LENGTH:
+            raise TooShortPrefix(f"The prefix \"{new_prefix}\" is too short! "
+                                 f"(the prefix must be greater then or equal "
+                                 f"to {MIN_PREFIX_LENGTH} character(s))")
+        if new_prefix in prefixes:
+            raise PrefixAlreadyExists(f"The prefix \"{new_prefix}\" already "
+                                      f"exists!")
+        for char in new_prefix:
+            if char not in ALLOWED_PREFIXES:
+                raise InvalidPrefix(f"The character \"{char}\" (in "
+                                    f"\"{new_prefix}\") is not a valid "
+                                    f"character for a prefix!")
+        if len(prefixes) + 1 > MAX_PREFIXES_PER_GUILD:
+            raise TooManyPrefixes(f"You are already at the maximum amount "
+                                  f"({MAX_PREFIXES_PER_GUILD}) of prefixes!")
+        prefixes.append(new_prefix)
+        guild["prefixes"] = prefixes
+        await self.set_guild(str(guild_id), guild)
+
+    async def remove_prefix(self, guild_id: int, old_prefix: str):
+        guild = await self.get_guild(str(guild_id))
+        prefixes = guild["prefixes"]
+        if old_prefix not in prefixes:
+            raise PrefixDoesNotExist(f"The prefix \"{old_prefix}\" isn't an "
+                                     f"existing prefix!")
+        if len(prefixes) - 1 > MIN_PREFIXES_PER_GUILD:
+            raise TooManyPrefixes(f"You are already at the minimum amount "
+                                  f"({MIN_PREFIXES_PER_GUILD}) of prefixes!")
+        prefixes.pop(prefixes.index(old_prefix))
+        guild["prefixes"] = prefixes
+        await self.set_guild(str(guild_id), guild)
--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -2,9 +2,35 @@ class ErinError(Exception):
     """Un-specified error"""
 
 
-class BadArguments(ErinError):
-    """Passed in not-enough arguments"""
+class PrefixError(ErinError):
+    """An error that involves the prefix"""
 
 
-class TooManyPrefixes(ErinError):
-    """Too many prefixes cause the max is 3"""
+class TooLongPrefix(PrefixError):
+    """The prefix to be added is too long"""
+
+
+class TooShortPrefix(PrefixError):
+    """The prefix to be added is too short"""
+
+
+class InvalidPrefix(PrefixError):
+    """The prefix is an invalid character"""
+
+
+class TooManyPrefixes(PrefixError):
+    """Adding the prefix will put you over the limit of the maximum amount of
+    prefixes"""
+
+
+class TooLittlePrefixes(PrefixError):
+    """Adding the prefix will put you under the limit of the minimum amount of
+    prefixes"""
+
+
+class PrefixAlreadyExists(PrefixError):
+    """This prefix already exists"""
+
+
+class PrefixDoesNotExist(PrefixError):
+    """This prefix does not exist"""
